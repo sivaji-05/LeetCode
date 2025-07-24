82abe070ea222d87f19797c4ef0e07793b9800db
Srinivasa Surya Sivaji <v.s.s.sivaji2005@gmail.com>
2025-07-24 23:48:12 +0530

    Time: 0 ms (100%), Space: 41.3 MB (81.5%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean stoneGame(int[] piles) {
+   /* int n=piles.length;
+    int left=0;
+    int right=n-1;
+    int alice=0;
+    int bob=0;
+    boolean aliceturn= true;
+    while(left<right){
+        int stone;
+        if(piles[left]>piles[right]){
+            stone=piles[left++];
+        }
+        else{
+            stone=piles[right--];
+        }
+        if(aliceturn){
+            alice+=stone;
+        }
+        else{
+            bob+=stone;
+        }
+        aliceturn=!aliceturn;
+    }
+    if(alice>bob){
+        return true;
+    }
+    return false;*/
+    return true;
+        
+    }
+}
